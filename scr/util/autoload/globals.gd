674555a66150b85fd94a1c4c6e1c913a5f2b3316
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-03-20 16:40:41 -0300

    Improve main menu GUI

--- scr/util/autoload/globals.gd
+++ scr/util/autoload/globals.gd
@@ -2,28 +2,42 @@ extends Node
 
 @onready var player_state: String
 @onready var player_speed: float = 3.0
-@onready var player: CharacterBody3D = get_tree().get_root().get_node("/root/Main/Player")
-@onready var player_animation_tree: AnimationTree = get_tree().get_root().get_node("/root/Main/Player/AnimationTree")
+@onready var player: CharacterBody3D = (
+	get_tree().get_root().get_node_or_null("/root/Main/Player") as CharacterBody3D
+)
+@onready var player_animation_tree: AnimationTree = (
+	get_tree().get_root().get_node_or_null("/root/Main/Player/AnimationTree") as AnimationTree
+)
 
-@onready var player_health: Health = player.health
-@onready var player_max_health: float = player_health.max_health
+var player_health: Health
+var player_max_health: float
 
-@onready var player_health_regen: float = player.health.health_regen
+var player_health_regen: float
 #@onready var player_mana_regen: float = player.health.mana_regen
-
 
 # TODO Move and refer mana to use generic mana component like Health
 #@onready var player_max_mana: float = maxf(5, Attributes.spirit * 50)
 #@onready var player_mana: float = player_max_mana:
-	#set(new_mana):
-		#player_mana = clampf(new_mana, 0, player_max_mana)
-	#get:
-		#return player_mana
+#set(new_mana):
+#player_mana = clampf(new_mana, 0, player_max_mana)
+#get:
+#return player_mana
 
 # TODO Either use this a global source of truth or a new GlobalInputController autoload
-@onready var mouse_mode : Input
+@onready var mouse_mode: Input
 
+
+# TODO Improve this check
 func _ready() -> void:
-	assert(player_animation_tree, "Player animation tree not defined in globals")
-	assert(player, "Player not defined in globals")
-	assert(player_health, "Player health resource not defined in globals")
+	if player == null:
+		push_error("Globals: Player not found. This is expected for UI-only scenes.")
+		return
+	if player_animation_tree == null:
+		push_error("Globals: Player animation tree not defined in globals.")
+		return
+	player_health = player.health
+	if player_health == null:
+		push_error("Globals: Player health resource not defined in globals.")
+		return
+	player_max_health = player_health.max_health
+	player_health_regen = player_health.health_regen
