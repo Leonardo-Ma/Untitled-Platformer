eef82f3c52099f15d80cd6a3511b6465d78c454d
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-03-23 20:23:56 -0300

    Refactor: Move all UI to its own node

--- scr/ui/menus/main_menu/new_game.gd
+++ scr/ui/menus/main_menu/new_game.gd
@@ -1,10 +1,7 @@
 extends Button
 
-const MAIN_SCENE_PATH: String = "uid://dlvcb7vmtq1lo"
-
 
 # TODO Remove this from signal gui and do it from code only
 func _on_pressed() -> void:
-	var main_scene: PackedScene = load(MAIN_SCENE_PATH)
+	var main_scene: PackedScene = load(UiManager.MAIN_SCENE_PATH)
 	get_tree().change_scene_to_packed(main_scene)
-	#owner.free()
