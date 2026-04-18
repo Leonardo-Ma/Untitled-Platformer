609744d05a8cc045e82f765230a11defeb0a2eab
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-04-18 00:27:58 -0300

    Fix: Improve HUD

--- scr/ui/button_animation.gd
+++ scr/ui/button_animation.gd
@@ -35,13 +35,16 @@ func _on_hover(button: Button) -> void:
 			SoundManager.SoundCategory.UI,
 		)
 	)
+	button.pivot_offset = button.size / 2.0
 	var tween: Tween = create_tween()
 	tween.tween_property(button, "scale", Vector2(1.05, 1.05), 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_property(button, "modulate", Color.DARK_ORANGE.darkened(0.2), 0.1)
 
 
 func _on_exit(button: Button) -> void:
 	var tween: Tween = create_tween()
 	tween.tween_property(button, "scale", Vector2(1, 1), 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_property(button, "modulate", Color.WHITE, 0.1)
 
 
 func _on_new_game_pressed() -> void:
