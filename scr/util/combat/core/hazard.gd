1faf2094568389f70b6c3c0b0bcedfb07028d455
Leonardo-Ma <68714883+Leonardo-Ma@users.noreply.github.com>
2026-04-19 10:55:31 -0300

    Feat: Improve spike trap
    Adds tween animation and activation by signal

--- scr/util/combat/core/hazard.gd
+++ scr/util/combat/core/hazard.gd
@@ -1,8 +1,31 @@
 class_name Hazard
-extends StaticBody3D
+extends Node3D
+
+signal activate
+signal deactivate
 
 @export var attack: Attack
 
+@onready var hitbox: Hitbox = %Hitbox
+
 
 func _ready() -> void:
-	assert(attack and attack.damage > 0 and attack.type != null, "Attack property incorrect for " + self.name)
+	assert(attack and attack.damage > 0 and attack.type != null, "Attack property incorrect for " + name)
+
+	if self.get_parent() is Area3D or StaticBody3D:
+		self.body_entered.connect(_on_body_entered)
+		self.body_exited.connect(_on_body_exited)
+	else:
+		assert(false, "I am not Area3D or StaticBody3D " + self.name)
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is PlayerEntity:
+		hitbox.find_child("CollisionShape3D").disabled = false
+		activate.emit()
+
+
+func _on_body_exited(body: Node3D) -> void:
+	if body is PlayerEntity:
+		hitbox.find_child("CollisionShape3D").disabled = true
+		deactivate.emit()
---
+++ scr/world_asset/dungeon_assets/individual_assets/spikes.gd
@@ -0,0 +1,22 @@
+class_name SpikeHazard
+extends Hazard
+
+@onready var spike_mesh: MeshInstance3D = $SpikeMesh
+@onready var spike_mesh_initial_position: Vector3 = spike_mesh.position
+
+
+func _ready() -> void:
+	super._ready()
+
+	activate.connect(_on_spike_up)
+	deactivate.connect(_on_spike_down)
+
+
+func _on_spike_up() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property(spike_mesh, "position:z", 0.0, 0.2)
+
+
+func _on_spike_down() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property(spike_mesh, "position:z", spike_mesh_initial_position.z, 0.2)
