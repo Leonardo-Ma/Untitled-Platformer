37fc04f294ef676b9966e13dd31d1729961e61e1
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-04-25 18:40:34 -0300

    Refactor: Improve folder structure
    Move all scripts, scenes and resources closer to respective feature
    
    Fix source folder typo

---
+++ src/entities/components/entity_debug_label.gd
@@ -0,0 +1,32 @@
+## This label is attached above entity that uses goap
+extends Label3D
+
+@onready var animation_tree: AnimationTree = %AnimationTree
+@onready var goap_controller: RedMageMemory = %GoapController
+
+@onready var label_text: String
+
+
+func _process(_delta: float) -> void:
+	if not self.visible:
+		return
+
+	var is_damaged: String = "Not damaged"
+
+	if animation_tree.get("parameters/is_damaged/active"):
+		is_damaged = "damaged"
+	label_text = is_damaged + " \n "
+	label_text += str(owner.health.health) + "\n"
+
+	label_text += "GOAP:\n"
+	#for values in goap_controller.get_blackboard().values():
+	#label_text += str(values) + "\n"
+
+	label_text += "Current goal: "
+	if owner.goap_agent._current_goal:
+		label_text += str(owner.goap_agent._current_goal.name) + "\n"
+
+	if owner.goap_agent._current_goal:
+		label_text += "Current goal: " + owner.goap_agent._current_goal.get_custom_class_name()
+
+	self.text = label_text
