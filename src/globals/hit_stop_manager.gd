3df89331e9cf466f1f5c3837865e61b75b8db03a
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-04-26 20:10:48 -0300

    Remove: Hit stop manager

--- src/globals/hit_stop_manager.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-## Will pause the game for duration when called
-func hit_stop(duration: float) -> void:
-	Engine.time_scale = 0
-	await get_tree().create_timer(duration, true, false, true).timeout
-	Engine.time_scale = 1
