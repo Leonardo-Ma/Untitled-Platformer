9cae7c3875dc0c0edd1c616d87b165a2d3b87b0a
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-05-03 17:22:07 -0300

    Refactor: Rename collectibles folder

--- src/systems/collectibles/collectible_data.gd
+++ src/systems/collectibles/collectible_data.gd
@@ -1,4 +1,4 @@
-## Base data container for collectibles. Use specific subclasses like CounterCollectible, StatusCollectibleData, or HealthCollectible.
+## Base data container for collectibles. Use specific subclasses like CounterCollectible, StatusCollectible, or HealthCollectible.
 @abstract class_name CollectibleData
 extends Resource
 
--- src/systems/collectibles/collectibles_data/status_collectible.gd
+++ src/systems/collectibles/collectibles_data/status_collectible.gd
@@ -1,4 +1,4 @@
-class_name StatusCollectibleData
+class_name StatusCollectible
 extends CollectibleData
 
 @export_category("Buff Mechanics")
@@ -8,10 +8,10 @@ extends CollectibleData
 func apply_effect(player: PlayerEntity) -> void:
 	assert(status_effect != null, "Status buff collectible " + str(identifier) + " missing effect resource.")
 
-	var p_entity: PlayerEntity = player as PlayerEntity
-	var status_manager: StatusManager = p_entity.status_manager
+	var entity: PlayerEntity = player as PlayerEntity
+	var status_manager: StatusManager = entity.status_manager
 
 	assert(status_manager.has_method("apply_status"), "StatusManager does not have apply_status method")
 	status_manager.apply_status(status_effect)
 
-	GameEvents.status_buff__collectible_collected.emit(status_effect, icon)
+	GameEvents.status_buff_collected.emit(status_effect, icon)
