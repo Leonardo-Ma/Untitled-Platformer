3eac3bc5bd3f144c62e89e46be2205883a072d6c
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-05-17 18:37:25 -0300

    Feat: Change default movement speed and jump height

--- src/entities/components/movement.gd
+++ src/entities/components/movement.gd
@@ -4,6 +4,6 @@
 class_name Movement
 extends Resource
 
-@export_range(3.0, 100.0, 0.1, "suffix:meters/second?") var walk_speed: float = 3.0
-@export_range(5.0, 150.0, 0.1, "suffix:meters/second?") var run_speed: float = 5.0
-@export_range(6.5, 150.0, 0.1, "suffix:meters/second?") var jump_velocity: float = 6.5
+@export_range(3.0, 100.0, 0.1, "suffix:meters/second?") var walk_speed: float = 5.5
+@export_range(5.0, 150.0, 0.1, "suffix:meters/second?") var run_speed: float = 5.5
+@export_range(6.5, 150.0, 0.1, "suffix:meters/second?") var jump_velocity: float = 7.0
