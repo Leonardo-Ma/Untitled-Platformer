d5177dd6e082b1764ea0d1f72dafce0df7b30fd3
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-05-19 11:53:13 -0300

    Chore: Small changes
    Feat: Add ground button visual effect
    
    Fix: Entrance platform floors
    
    Chore: Change power up height

--- src/environment/levels/assets/ground_button/button.gd
+++ src/environment/levels/assets/ground_button/button.gd
@@ -3,6 +3,8 @@ extends Area3D
 signal button_toggled_on
 signal button_toggled_off
 
+@onready var button_mesh: MeshInstance3D = $ButtonMesh
+
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -12,6 +14,8 @@ func _on_body_entered(_body: Node3D) -> void:
 	if _body is PlayerEntity:
 		button_toggled_on.emit()
 		set_deferred("monitoring", false)
+		button_mesh.global_position.y -= 0.120
 		await get_tree().create_timer(4).timeout
+		button_mesh.global_position.y += 0.120
 		set_deferred("monitoring", true)
 		button_toggled_off.emit()
