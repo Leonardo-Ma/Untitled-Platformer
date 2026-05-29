186f2d001939448f3c90d6f5a58caf9772125460
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-05-29 17:04:20 -0300

    Chore: Add icon assert to power ups

--- src/environment/levels/assets/collectibles/unlock_skill/unlock_skill.gd
+++ src/environment/levels/assets/collectibles/unlock_skill/unlock_skill.gd
@@ -6,5 +6,6 @@ func _ready() -> void:
 	collect_sounds = [
 		preload("uid://cag33c6eom3kf"),  # harpsichord_chime_positive.wav
 	]
+	assert(self.data.definition, "Skill not defined by + " + owner.name)
 
 # TODO Add a festive animation effect when picking up this (tween?)
