d2234d04a66c0a81ade67246ad03429b1508ec50
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-06-15 15:17:50 -0300

    Remove: Redundant mouse capture in main

--- src/main.gd
+++ src/main.gd
@@ -4,8 +4,3 @@ extends Node
 func _ready() -> void:
 	LevelManager.initialize_level(self)
 	SoundManager.change_music_state(MusicController.MusicState.EXPLORATION, false, "blossom")
-
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
