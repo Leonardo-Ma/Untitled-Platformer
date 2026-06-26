58e96016a40711e64323fc74375b033adc5ff87f
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-06-25 22:36:13 -0300

    Fix: Improve unlock skill assert check

--- src/environment/levels/assets/collectibles/unlock_skill/unlock_skill.gd
+++ src/environment/levels/assets/collectibles/unlock_skill/unlock_skill.gd
@@ -5,6 +5,6 @@ func _child_ready() -> void:
 	collect_sounds = [
 		preload("uid://cag33c6eom3kf"),  # harpsichord_chime_positive.wav
 	]
-	assert(self.data.definition, "Skill not defined by + " + owner.name)
-
+	var skill_data: SkillCollectible = data as SkillCollectible
+	assert(skill_data != null and skill_data.definition != null, "UnlockSkill: SkillCollectible with a valid definition required on " + name)
 # TODO Add a festive animation effect when picking up this (tween?)
