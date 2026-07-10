5148755db654bee97787a9ad46eee776d16ac4e2
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-07-10 12:51:14 -0300

    Feat: Improve car camera follow
    Fix: Improve car tuning

--- src/entities/vehicles/car.gd
+++ src/entities/vehicles/car.gd
@@ -1,3 +1,4 @@
+# https://www.youtube.com/watch?v=5m7nBj98rx4 LegionGames - Race Car Controller Tutorial - Godot 3D
 class_name PlayerCar
 extends VehicleBody3D
 
@@ -5,9 +6,16 @@ signal driving_started(player: PlayerEntity)
 signal driving_stopped(player: PlayerEntity)
 signal teleported  # Back to checkpoint for example
 
+## Peak engine force at full throttle [br]
+## How fast the car can go [br] Applied per traction wheel
 @export_range(500.0, 8000.0, 100.0) var max_engine_force: float = 1200.0
+# TODO Add a conditional to balance engine force and brake force
+## Brake force applied when not accelerating
 @export_range(5.0, 100.0, 1.0) var brake_force: float = 7.0
+## How much angle the wheels can turn in radians
 @export_range(0.1, 1.0, 0.01) var max_steering: float = 0.1
+# TODO Add a conditional to balance max steering angle and speed
+## Steering angle change rate
 @export_range(0.1, 5.0, 0.1) var steering_speed: float = 0.8
 
 @export_category("Core")
@@ -23,6 +31,7 @@ var _steering: float = 0.0
 @onready var camera_anchor: Marker3D = %CameraAnchor
 @onready var camera_pivot: Node3D = %CameraPivot
 @onready var camera: Camera3D = %Camera3D
+@onready var camera_look_at: Vector3 = global_position
 
 @onready var front_left_wheel: VehicleWheel3D = %FrontLeftWheel
 @onready var front_right_wheel: VehicleWheel3D = %FrontRightWheel
@@ -65,18 +74,21 @@ func _physics_process(delta: float) -> void:
 		engine_force = 0.0
 		brake = brake_force
 
-
-func _process(_delta: float) -> void:
-	camera_pivot.global_position = camera_anchor.global_position
+	camera_pivot.global_position = camera_pivot.global_position.lerp(camera_anchor.global_position, delta * 20.0)
+	camera_pivot.global_transform.basis = camera_pivot.global_transform.basis.slerp(camera_anchor.global_transform.basis, delta * 5.0)
+	var target: Vector3 = global_position + (global_transform.basis.z * 10.0)
+	camera_look_at = camera_look_at.lerp(target, delta * 5.0)
+	camera.look_at(camera_look_at)
 
 
 func _on_enter_area_body_entered(body: Node3D) -> void:
-	if is_driven or body is not PlayerEntity:
+	if (is_driven) or (body is not PlayerEntity):
 		return
 	_driver = body as PlayerEntity
 	_driver.camera_controller.set_active(false)
 
 	is_driven = true
+	enter_area.set_deferred("monitoring", false)
 	_driver.enter_vehicle()
 
 	camera.current = true
@@ -103,6 +115,8 @@ func exit(exit_position: Vector3) -> void:
 	_driver.camera_controller.set_active(true)
 	_driver = null
 	is_driven = false
+
+	enter_area.set_deferred("monitoring", true)
 
 	driver.exit_vehicle(exit_position)
 
