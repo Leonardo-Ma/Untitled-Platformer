5e997987fb1e5143498aec6f2c7706045b78bfa4
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-07-10 15:51:36 -0300

    Feat: Add door open and close sound

--- src/environment/levels/assets/doors/large_door.gd
+++ src/environment/levels/assets/doors/large_door.gd
@@ -1,6 +1,9 @@
 class_name LargeDoor
 extends Node3D
 
+const DOOR_OPEN = preload("uid://cx5j2ge2f0m6q")
+const DOOR_CLOSE = preload("uid://diaumdug5gwrg")
+
 @export var _button: Area3D = null
 
 @onready var door: MeshInstance3D = $Door
@@ -13,7 +16,9 @@ func _ready() -> void:
 
 func open() -> void:
 	door.rotation.y = -90
+	SoundManager.play_sound(DOOR_OPEN, SoundManager.SoundCategory.SFX, global_position)
 
 
 func close() -> void:
 	door.rotation.y = 0
+	SoundManager.play_sound(DOOR_CLOSE, SoundManager.SoundCategory.SFX, global_position)
